2ccabc2ef1943c2140688277e5e5d01e75ec8369
MatthiasL <matthias_ludwig@yahoo.com>
2016-07-28 06:17:03 +1000

    https://angular.io/docs/ts/latest/tutorial/toh-pt6.html hero-search.component.ts started

--- tourofheroes/new/app/hero.service.ts
+++ tourofheroes/new/app/hero.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'app/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
+
+
   getHeroes() {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number) {
@@ -13,11 +23,66 @@ export class HeroService {
       .then(heroes => heroes.find(hero => hero.id === id));
   }
 
+  // Add new Hero
+  private post(hero: Hero): Promise<Hero> {
+    let headers = new Headers({
+      'Content-Type': 'application/json'
+    });
 
-  getHeroesSlowly() {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-    );
+    return this.http
+      .post(this.heroesUrl, JSON.stringify(hero), { headers: headers })
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
   }
+
+  // Update existing Hero
+  private put(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+      .put(url, JSON.stringify(hero), { headers: headers })
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  delete(hero: Hero) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http
+      .delete(url, { headers: headers })
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  save(hero: Hero): Promise<Hero> {
+    if (hero.id) {
+      return this.put(hero);
+    }
+    return this.post(hero);
+  }
+
+
+
+
+
+  //getHeroesSlowly() {
+  //  return new Promise<Hero[]>(resolve =>
+  //    setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+  //  );
+  //}
+
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
 
 }
