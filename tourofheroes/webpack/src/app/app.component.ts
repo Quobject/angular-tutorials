bfd106487adba4e058142856641985ebea384171
MatthiasL <matthias_ludwig@yahoo.com>
2016-07-29 03:51:16 +1000

    tourofheroes webpack start

---
+++ tourofheroes/webpack/src/app/app.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { TasklistComponent } from './tasklist.component';
+import { Task1Component } from './task1.component';
+import { Task2Component } from './task2.component';
+
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  precompile: [TasklistComponent, Task1Component, Task2Component]
+})
+export class AppComponent { }
