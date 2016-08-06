c3ab49c09e9a29a314d225c342468c5d0de505d3
MatthiasL <matthias_ludwig@yahoo.com>
2016-08-07 08:09:45 +1000

    reverse

--- golang/src/github.com/mattqs/hello/hello.go
+++ golang/src/github.com/mattqs/hello/hello.go
@@ -5,6 +5,11 @@ import (
   "github.com/mattqs/stringutil"
 )
 
+type t struct {
+    name string // name of the object
+    value int // its value
+}
+
 func main() {
     fmt.Printf( stringutil.Reverse("Hello, new gopher!"))
 }
