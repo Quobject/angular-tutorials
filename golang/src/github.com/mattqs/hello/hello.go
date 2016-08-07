d4e53d5e1cb075404dd5af32d682b85becbe8b0e
MatthiasL <matthias_ludwig@yahoo.com>
2016-08-07 12:47:44 +1000

    upandrunning Watched: Getting input from the console

--- golang/src/github.com/mattqs/hello/hello.go
+++ golang/src/github.com/mattqs/hello/hello.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "fmt"  
-  "github.com/mattqs/stringutil"
+	"fmt"
+	"github.com/mattqs/stringutil"
 )
 
 type t struct {
-    name string // name of the object
-    value int // its value
+	name  string // name of the object
+	value int    // its value
 }
 
 func main() {
-    fmt.Printf( stringutil.Reverse("Hello, new gopher!"))
+	fmt.Println(stringutil.Reverse("Hello, new gopher!"))
 }
